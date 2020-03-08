9fd6e8256a3c75a376154f28b3183cb30f86dd56
Logic <1045382156@qq.com>
2020-03-08 18:17:01 +0800

    首页页面部分完善

--- weixin/pages/home/home.js
+++ weixin/pages/home/home.js
@@ -4,23 +4,27 @@ import http from '../../utils/http.js'
 import apis from '../../config/api.js'
 const app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    // banner分类
+    // banner数据
+    bannerList: [],
+    // banner下方的分类
     classifyList: [],
-    // 页面底部的下拉数据
-    pageData: [],
+    // 商品类型
+    goodsTypeData: [],
     // 页面数据查询参数
     pageQuery: {
+      // 当前页面查询的数据类型
       type: 0,
       page: 1,
       size: 10
     },
-    // 商品类型
-    goodsType: [],
+    // 页面底部的下拉数据
+    pageData: [],
+    // 商品id数量map
+    goodsNumMap: {},
     shop: []
   },
 
@@ -28,15 +32,139 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 获取商户列表
+    this.getShopList()
+    // 初始化分类数据
     this.getClassifyData()
+    // 初始化获取分类
     this.getGoodsType()
-    this.getShopList();
+    // 获取banner
+    this.getBannerList()
+    // 小程序没法用async函数，页面加载时，用promise保证请求完购物车数据再请求页面商品数据
+    new Promise((resolve, reject) => {
+      // 获取购物车数据
+      this.getShopcarData()
+      resolve()
+    }).then(() => {
+      // 获取页面商品数据——购物车数据获取完成后再获取页面数据，方便页面中已购物数量的渲染
+      this.getPageData()
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
     
+  },
+  // 页面上拉触底事件的处理函数
+  onReachBottom () {
+
+  },
+  // 用户输入完成点击搜索
+  confirmSearch (e) {
+    console.log('点击搜索触发的事件对象', e)
+    console.log('用户输入:', e.detail.value)
+  },
+  // 点击banner
+  tapBanner (e) {
+    console.log('点击banner:', e.currentTarget.dataset)
+  },
+  // 点击分类
+  tapClassify (e) {
+    console.log('点击分类:', e.currentTarget.id)
+  },
+  // 点击商品分类
+  tapGoodsType (e) {
+    console.log('点击商品分类:', e.currentTarget.dataset.type)
+    let type = e.currentTarget.dataset.type
+    this.setData({
+      'pageQuery.type': type
+    })
+    this.getPageData()
+  },
+  // 点击商品非购物车按钮——跳转至详情页
+  goGoodsDetail (e) {
+    console.log('去商品详情页', e)
+  },
+  // 点击减少
+  reduceShopcar (e) {
+    console.log('购物车减少', e.target.dataset)
+    let {
+      id,
+      index
+    } = e.target.dataset
+  },
+  // 点击增加
+  addShopcar (e) {
+    console.log('购物车增加', e.target.dataset)
+    let {
+      id,
+      index,
+      surplus
+    } = e.target.dataset
+  },
+  // 点击购物车图标增加
+  addShopcar2 (e) {
+    console.log('购物车增加', e.currentTarget.dataset)
+    let {
+      id,
+      index,
+      surplus
+    } = e.currentTarget.dataset
+  },
+  // 页面加载数据
+  getPageData () {
+    let pageQuery = this.data.pageQuery
+    let {
+      type,
+      page,
+      size
+    } = pageQuery
+    let goodsNumMap = this.data.goodsNumMap
+    console.log('goodsNumMap:', goodsNumMap)
+    let pageData = []
+    for (let i = (page - 1) * size; i < page * size; i++ ) {
+      pageData.push({
+        id: i,
+        type,
+        goodsImg: '../../images/test/2.jpg',
+        title: `货物${i}`,
+        desc: `货物${i}描述文字`,
+        num: goodsNumMap[i] || 0,
+        solded: parseInt(Math.random() * 20),
+        surplus: parseInt(Math.random() * 20),
+        price: parseInt(Math.random() * 100),
+        oldPrice: parseInt(Math.random() * 100)
+      })
+    }
+    let _pageData = this.data.pageData
+    _pageData = page === 1 ? pageData : _pageData.concat(pageData)
+    this.setData({
+      pageData: _pageData
+    })
+  },
+  // 获取购物车数据
+  getShopcarData () {
+    let shopcarData = [
+      {
+        id: 1,
+        num: 3
+      },
+      {
+        id: 2,
+        num: 10
+      },
+      {
+        id: 3,
+        num: 6
+      }
+    ]
+    let goodsNumMap = {}
+    shopcarData.map(item => {
+      goodsNumMap[item.id] = item.num
+    })
+    // 不在页面中渲染的数据，可以直接修改data
+    this.data.goodsNumMap = goodsNumMap
   },
   // 获取商户列表
   getShopList: function () {
@@ -64,32 +192,32 @@ Page({
     let classifyList = [
       {
         id: 1,
-        icon: 'http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=1035415831,1465727770&os=2036467054,2328224179&simid=4030878874,470441821&pn=0&rn=1&di=7370&ln=1698&fr=&fmq=1583645701133_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&hs=2&objurl=http%3A%2F%2Fa3.att.hudong.com%2F68%2F61%2F300000839764127060614318218_950.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined',
+        icon: '../../images/test/1.jpg',
         txt: '分类1'
       },
       {
         id: 2,
-        icon: 'http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=1906469856,4113625838&os=1062705421,520912533&simid=3285371631,209838447&pn=1&rn=1&di=8250&ln=1698&fr=&fmq=1583645701133_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&hs=2&objurl=http%3A%2F%2Fa2.att.hudong.com%2F36%2F48%2F19300001357258133412489354717.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined',
+        icon: '../../images/test/2.jpg',
         txt: '分类2'
       },
       {
         id: 3,
-        icon: 'http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=3173584241,3533290860&os=1133571898,402444249&simid=3493630544,246115770&pn=2&rn=1&di=180400&ln=1698&fr=&fmq=1583645701133_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&hs=2&objurl=http%3A%2F%2Fa0.att.hudong.com%2F78%2F52%2F01200000123847134434529793168.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined',
+        icon: '../../images/test/3.jpg',
         txt: '分类3'
       },
       {
         id: 4,
-        icon: 'http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=2534506313,1688529724&os=1097436471,408122739&simid=3354786982,133358663&pn=3&rn=1&di=177760&ln=1698&fr=&fmq=1583645701133_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&hs=2&objurl=http%3A%2F%2Fa3.att.hudong.com%2F14%2F75%2F01300000164186121366756803686.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined',
+        icon: '../../images/test/4.jpg',
         txt: '分类4'
       },
       {
         id: 5,
-        icon: 'http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=1208538952,1443328523&os=979813124,369989102&simid=62453799,522850401&pn=4&rn=1&di=9240&ln=1698&fr=&fmq=1583645701133_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&hs=2&objurl=http%3A%2F%2Fa4.att.hudong.com%2F21%2F09%2F01200000026352136359091694357.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined',
+        icon: '../../images/test/5.jpg',
         txt: '分类5'
       },
       {
         id: 6,
-        icon: 'http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%BE%E7%89%87&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=4018557288,1217151095&os=786175811,213722211&simid=3476599800,394564942&pn=5&rn=1&di=11770&ln=1698&fr=&fmq=1583645701133_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&hs=2&objurl=http%3A%2F%2Fa0.att.hudong.com%2F27%2F10%2F01300000324235124757108108752.jpg&rpstart=0&rpnum=0&adpicid=0&force=undefined',
+        icon: '../../images/test/6.jpg',
         txt: '分类6'
       }
     ]
@@ -97,8 +225,9 @@ Page({
       classifyList
     })
   },
+  // 初始化获取分类
   getGoodsType () {
-    let goodsType = [
+    let goodsTypeData = [
       {
         type: 0,
         txt: '全部'
@@ -114,10 +243,38 @@ Page({
       {
         type: 3,
         txt: '类型3'
+      },
+      {
+        type: 4,
+        txt: '类型4'
+      },
+      {
+        type: 5,
+        txt: '类型5'
+      },
+      {
+        type: 6,
+        txt: '类型6'
       }
     ]
     this.setData({
-      goodsType
+      goodsTypeData
+    })
+  },
+  // 获取banner
+  getBannerList () {
+    let bannerList = [
+      {
+        id: 1,
+        src: '../../images/test/7.jpg'
+      },
+      {
+        id: 2,
+        src: '../../images/test/5.jpg'
+      }
+    ]
+    this.setData({
+      bannerList
     })
   }
 })
